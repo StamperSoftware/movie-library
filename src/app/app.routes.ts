503c06fd88f61fe0ff1ead478edbd902c6b9b298
StamperSoftware <StamperSoftware@gmail.com>
2024-08-03 14:14:54 -0400

    Init site

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {HomeComponent} from "./home/home.component";
+
+export const routes: Routes = [
+  { path:"", component:HomeComponent },
+  { path:"**", component:PageNotFoundComponent },
+];
+
